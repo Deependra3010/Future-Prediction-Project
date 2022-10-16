6110e0663187cf62094c21a9e7d104eafcd335b9
Deependra Solanki <deepsolanki3010@gmail.com>
2022-10-16 14:20:47 +0530

    completed all pages remaining to add city images and graph

--- js/prediction.js
+++ js/prediction.js
@@ -1,9 +1,9 @@
 const hamBurger = document.querySelector('.hamBurger');
 const navBar = document.querySelector('.navBar');
-const rightSide = document.querySelector('.rightSide')
+const predictionRightSide = document.querySelector('.predictionRightSide');
 // const rightSide
 hamBurger.addEventListener('click', () => {
     hamBurger.classList.toggle('active');
     navBar.classList.toggle('widhtNoneToggle');
-    rightSide.classList.toggle('displayNoneToggle')
+    predictionRightSide.classList.toggle('displayNoneToggle')
 })
